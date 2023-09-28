82a0fa72b5ba90d65b46d5441f12a585363eedd0
Lombard Jérémy <jeremylombard.mail@gmail.com>
2023-09-29 01:44:30 +0200

    microchanges

--- Assets/Scripts/PlayerManager.cs
+++ Assets/Scripts/PlayerManager.cs
@@ -2,8 +2,23 @@ using UnityEngine;
 
 public class PlayerManager : MonoBehaviour
 {
+    //Champs
+    public long souls;
+    
+
+    //Methodes
     public void OnClickInClickArea()
     {
         Debug.Log("Click in the good spot");
+    }
+
+    public void AddSouls(long amount)
+    {
+        souls += amount;
+    }
+
+    public void ConsumeSouls(long amount)
+    {
+        souls -= amount;
     }
 }
