579cff28dfffdc9ec938647dc87b2ec62530d7c0
Lombard Jérémy <jeremylombard.mail@gmail.com>
2024-01-23 03:54:02 +0100

    Je veux mourir

--- Assets/Scripts/EquipmentGenerator.cs
+++ Assets/Scripts/EquipmentGenerator.cs
@@ -29,7 +29,23 @@ public class EquipmentGenerator : MonoBehaviour
 
     public void GenerateItem()
     {
-        int randomIndex = Random.Range(0, DatabasesManager.Instance.EquipementDatabase.EquipementDatas.Count);
+        int _radValue = Random.Range(0,3);
+        int randomIndex = 0;
+        switch (_radValue)
+        {
+            case 0:
+                randomIndex = Random.Range(0, 12);
+                break;
+            case 1:
+                randomIndex = Random.Range(13, 26);
+                break;
+            case 2:
+                randomIndex = Random.Range(26, 39);
+                break;
+            default:
+                Debug.Log("Bug");
+                break;
+        }
         EquipementData equipmentDropped = DatabasesManager.Instance.EquipementDatabase.EquipementDatas[randomIndex];
         equipmentDropped.Id = randomIndex;
         Inventory.Instance.ItemInventory.Add(equipmentDropped);
